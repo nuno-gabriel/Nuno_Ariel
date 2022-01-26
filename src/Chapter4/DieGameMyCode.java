4bea42026b014927d04673a870c85f92eb270da4
nuno-gabriel <98390802+nuno-gabriel@users.noreply.github.com>
2022-01-26 16:03:31 +0000

    A lot of changes

---
+++ src/Chapter4/DieGameMyCode.java
@@ -0,0 +1,44 @@
+package Chapter4;
+
+import java.util.Random;
+
+public class DieGameMyCode {
+
+    public static void main (String args[]) {
+        int Victory = 20;
+        int currentposition = 0;
+        int tries = 5;
+
+        Random random = new Random();
+
+        System.out.println("Hello, welcome to this game. Do you want to play?");
+
+
+
+         for (int i = 1; i <= tries; i++) {
+
+            int die = random.nextInt(6) + 1;
+            currentposition = currentposition + die;
+
+            System.out.print(String.format("Roll #%d: You've rolled a %d. ", i, die));
+
+            if (currentposition == Victory) {
+                System.out.print("You're on space " + currentposition + ". Congrats, you win!");
+                break;
+            } else if (currentposition > Victory) {
+                System.out.print("Unfortunately, that takes you past " + Victory + " spaces. You lose!");
+                break;
+            } else if (i == tries && currentposition < Victory) {
+                System.out.println("You're on space " + currentposition + ".");
+                System.out.println("Unfortunately, you didn't make it to all " +
+                        currentposition + " spaces. You lose!");
+            } else {
+                int spacesToGo = Victory - currentposition;
+                System.out.print("You are now on space " + currentposition +
+                        " and have " + spacesToGo + " more to go.");
+            }
+
+            System.out.println();
+        }
+    }
+}
