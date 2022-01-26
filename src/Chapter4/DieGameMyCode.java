712833a959f0954ffdf821b1fc97c1a760508361
nuno-gabriel <98390802+nuno-gabriel@users.noreply.github.com>
2022-01-26 16:15:36 +0000

    Dice my game

---
+++ src/Chapter4/DieGameMyCode.java
@@ -0,0 +1,51 @@
+package Chapter4;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class DieGameMyCode {
+
+    public static void main(String args[]) {
+        int Victory = 20;
+        int currentposition = 0;
+        int tries = 5;
+
+        Random random = new Random();
+
+        System.out.println("Hello, welcome to this game. Do you want to play?(type true or false)");
+        Scanner scanner = new Scanner(System.in);
+        boolean answer = scanner.nextBoolean();
+        if (answer) {
+            scanner.close();
+
+
+            for (int i = 1; i <= tries; i++) {
+
+                int die = random.nextInt(6) + 1;
+                currentposition = currentposition + die;
+
+                System.out.print(String.format("Roll #%d: You've rolled a %d. ", i, die));
+
+                if (currentposition == Victory) {
+                    System.out.print("You're on space " + currentposition + ". Congrats, you win!");
+                    break;
+                } else if (currentposition > Victory) {
+                    System.out.print("Unfortunately, that takes you past " + Victory + " spaces. You lose!");
+                    break;
+                } else if (i == tries && currentposition < Victory) {
+                    System.out.println("You're on space " + currentposition + ".");
+                    System.out.println("Unfortunately, you didn't make it to all " +
+                            currentposition + " spaces. You lose!");
+                } else {
+                    int spacesToGo = Victory - currentposition;
+                    System.out.print("You are now on space " + currentposition +
+                            " and have " + spacesToGo + " more to go.");
+                }
+
+                System.out.println();
+            }
+        } else {
+            System.out.println("Well then. GoodBye");
+        }
+    }
+}
