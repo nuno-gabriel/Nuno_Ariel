499fef145423c96f772ddb685629a0c7185b1303
asromero2018 <asromero2018@gmail.com>
2022-01-26 12:03:06 +0100

    commit_Main26.01

--- src/Chapter4/GrossPayInputValidation.java
+++ src/Chapter4/GrossPayInputValidation.java
@@ -11,7 +11,7 @@ public class GrossPayInputValidation {
 
 
         //Get input for unknown variables
-        System.out.println("How many hours did you work this week?");
+        System.out.println("days of the week*****************????");
         Scanner scanner = new Scanner(System.in);
         double hoursWorked = scanner.nextDouble();
 
